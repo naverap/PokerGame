c60ed3a2128e81181a115c149800c485f88d7da2
Nave Rap <naveapp@gmail.com>
2023-05-21 18:11:26 +0300

    j

---
+++ PokerGame/SignInActivity.cs
@@ -0,0 +1,73 @@
+﻿//using Android.App;
+//using Android.OS;
+//using Android.Views;
+//using Android.Widget;
+//using PokerGame;
+
+//namespace YourPackageName
+//{
+//    [Activity(Label = "SignInActivity")]
+//    public class SignInActivity : Activity
+//    {
+//        private EditText usernameEditText;
+//        private EditText passwordEditText;
+//        private Button signInButton;
+//        private TextView signUpTextView;
+
+//        protected override void OnCreate(Bundle savedInstanceState)
+//        {
+//            base.OnCreate(savedInstanceState);
+//            SetContentView(Resource.Layout.activity_sign_in);
+
+//            usernameEditText = FindViewById<EditText>(Resource.Id.usernameEditText);
+//            passwordEditText = FindViewById<EditText>(Resource.Id.passwordEditText);
+//            signInButton = FindViewById<Button>(Resource.Id.signinButton);
+//            signUpTextView = FindViewById<TextView>(Resource.Id.signupTextView);
+
+//            signInButton.Click += SignInButton_Click;
+//            signUpTextView.Click += SignUpTextView_Click;
+//        }
+
+//        private void SignInButton_Click(object sender, System.EventArgs e)
+//        {
+//            SignIn();
+//        }
+
+//        private void SignUpTextView_Click(object sender, System.EventArgs e)
+//        {
+//            // Start the SignUpActivity
+//            StartActivity(typeof(SignUpActivity));
+//        }
+
+//        private void SignIn()
+//        {
+//            string username = usernameEditText.Text;
+//            string password = passwordEditText.Text;
+
+//            // Perform sign-in logic here
+//            // Validate username and password
+
+//            if (IsValidSignIn(username, password))
+//            {
+//                // Show a toast message indicating successful sign-in
+//                Toast.MakeText(this, "Sign in successful!", ToastLength.Short).Show();
+
+//                // Start the MainActivity or perform any desired action
+//                StartActivity(typeof(MainActivity));
+//                Finish();
+//            }
+//            else
+//            {
+//                // Show a toast message indicating invalid credentials
+//                Toast.MakeText(this, "Invalid username or password!", ToastLength.Short).Show();
+//            }
+//        }
+
+//        private bool IsValidSignIn(string username, string password)
+//        {
+//            // Implement your own sign-in validation logic here
+//            // For demonstration purposes, assume sign-in is successful if the username and password are both "admin"
+//            return (username == "admin" && password == "admin");
+//        }
+//    }
+//}
--- PokerGame/SignUpActivity.cs
+++ PokerGame/SignUpActivity.cs
@@ -1,15 +1,9 @@
 ﻿//using Android.App;
-//using Android.Content;
 //using Android.OS;
-//using Android.Runtime;
 //using Android.Views;
 //using Android.Widget;
-//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
 
-//namespace PokerGame
+//namespace YourPackageName
 //{
 //    [Activity(Label = "SignUpActivity")]
 //    public class SignUpActivity : Activity
@@ -20,14 +14,48 @@
 //        private EditText reEnterPasswordEditText;
 //        private Button signUpButton;
 //        private TextView signInTextView;
+
 //        protected override void OnCreate(Bundle savedInstanceState)
 //        {
 //            base.OnCreate(savedInstanceState);
-//            SetContentView(Resource.Layout.Signup);
-//            usernameEditText = (EditText)FindViewById(Resource.Id.);
+//            SetContentView(Resource.Layout.activity_sign_up);
 
+//            usernameEditText = FindViewById<EditText>(Resource.Id.usernameEditText);
+//            gmailEditText = FindViewById<EditText>(Resource.Id.GmailEditText);
+//            passwordEditText = FindViewById<EditText>(Resource.Id.passwordEditText);
+//            reEnterPasswordEditText = FindViewById<EditText>(Resource.Id.reEnterPasswordEditText);
+//            signUpButton = FindViewById<Button>(Resource.Id.signinButton);
+//            signInTextView = FindViewById<TextView>(Resource.Id.signinTextView);
 
-//            // Create your application here
+//            signUpButton.Click += SignUpButton_Click;
+//            signInTextView.Click += SignInTextView_Click;
+//        }
+
+//        private void SignUpButton_Click(object sender, System.EventArgs e)
+//        {
+//            SignUp();
+//        }
+
+//        private void SignInTextView_Click(object sender, System.EventArgs e)
+//        {
+//            Finish();
+//        }
+
+//        private void SignUp()
+//        {
+//            string username = usernameEditText.Text;
+//            string gmail = gmailEditText.Text;
+//            string password = passwordEditText.Text;
+//            string reEnterPassword = reEnterPasswordEditText.Text;
+
+//            // Perform sign-up logic here
+//            // Validate input and save user data
+
+//            // Show a toast message indicating successful sign-up
+//            Toast.MakeText(this, "Sign up successful!", ToastLength.Short).Show();
+
+//            // Finish the activity and go back to sign-in page
+//            Finish();
 //        }
 //    }
 //}
